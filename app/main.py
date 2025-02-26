6532886cb62f41b2f93be76e3575668353de90fd
Elisha <elishagjoy98@gmail.com>
2025-02-26 14:27:04 +0530

    AddedCrud routers for project,space and work_type..Also changed import path in all models for BaseModel

--- app/main.py
+++ app/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.routers import project, work_type
+
 
 app = FastAPI()
 
-@app.route("/")
-def read_route():
-    return {"messsage":"Welcome to DeTaska API 🚀"}
+
+# Include routers
+app.include_router(project.router)
+app.include_router(work_type.router)
+
+@app.get("/")
+def read_root():
+    return {"message": "DeTaska Backend is Running!"}
