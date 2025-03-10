435db172d45867921f0ea60afcd8cc9a7fd424fc
Elisha <elishagjoy98@gmail.com>
2025-03-10 16:59:45 +0530

    fix(models): Ensure updated_at auto-updates on record changes

--- app/models/base.py
+++ app/models/base.py
@@ -9,8 +9,7 @@ class BaseModelWithTimestamps(SQLModel):
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
     updated_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
-# Automatically update `updated_at` before an update occurs
-def set_updated_at(mapper, connection, target):
-    target.updated_at = datetime.utcnow()
 
-event.listen(BaseModelWithTimestamps, "before_update", set_updated_at)
+@event.listens_for(BaseModelWithTimestamps, "before_update", propagate=True)
+def update_timestamp(mapper, connection, target):
+    target.updated_at = datetime.utcnow()
